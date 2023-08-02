355ed9896806514f5d3bc97aeaa9929070e3bd3a
Piyaporn <piyapornlahr@gmail.com>
2023-08-02 13:28:09 +0100

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Stock from "./pages/Stock/Stock";
+import Crypto from "./pages/Crypto/Crypto";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stock" element={<Stock />} />
+          <Route path="/crypto" element={<Crypto />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
