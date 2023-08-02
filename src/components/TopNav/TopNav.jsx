e0d1241100ed6b536a0b0f1044da7d48ba9b1b0c
Piyaporn <piyapornlahr@gmail.com>
2023-08-02 14:26:00 +0100

    Top navigator bar created

---
+++ src/components/TopNav/TopNav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./TopNav.scss";
+import SearchBar from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
+
+const TopNav = () => {
+  return (
+    <div className="topNav">
+      <div className="topNav__nav-items">
+        <Link to="/">
+          <div className="topNav__nav-items--app-name">
+            <h1 className="app-name">TICKR</h1>
+          </div>
+        </Link>
+
+        <div className="topNav__nav-items--options">
+          <Link to="/stock">
+            <p className="text">STOCKS</p>
+          </Link>
+          <Link to="/crypto">
+            <p className="text">CRYPTO</p>
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <SearchBar />
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
