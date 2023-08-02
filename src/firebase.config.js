cc81db724fdb152cb1e3851fe99f5d735f4a85e7
Piyaporn <piyapornlahr@gmail.com>
2023-08-02 12:56:47 +0100

    Initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcQvN5bRSMNDzDPV-seDc6BS-h50FxOiw",
+  authDomain: "stocks-crypto-tracker.firebaseapp.com",
+  projectId: "stocks-crypto-tracker",
+  storageBucket: "stocks-crypto-tracker.appspot.com",
+  messagingSenderId: "33879499220",
+  appId: "1:33879499220:web:255f16b6f6dac2e5083641"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
