084944a11dd3dd739891065f92d63e6303e73b01
Piyaporn <piyapornlahr@gmail.com>
2023-08-02 16:39:49 +0100

    debugged on a styling

--- src/pages/Crypto/Crypto.jsx
+++ src/pages/Crypto/Crypto.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Crypto.scss";
 import TopNav from "../../components/TopNav/TopNav";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import StocksBar from "../../components/StocksBar/StocksBar";
 
 const Crypto = () => {
   return (
@@ -9,6 +10,9 @@ const Crypto = () => {
       <div className="crypto-page__topNav">
         <TopNav page="others" textColor="black" textColor2="black" />
         <SearchBar />
+      </div>
+      <div>
+        <StocksBar />
       </div>
     </div>
   );
