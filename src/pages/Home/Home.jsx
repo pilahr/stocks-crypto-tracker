a0aba151ac7aa0f44c6e775f58f393baf119ac99
Piyaporn <piyapornlahr@gmail.com>
2023-08-05 10:26:36 +0100

    image on homepage changed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         </div>
         <div className="homepage__appPicture">
           <img className="homepage__appPicture--appPic"
-            src="https://opusrs-my.sharepoint.com/:i:/r/personal/piyaporn_lahr_nology_io/Documents/Photos%20for%20project/61533EFA-5EC9-4198-ABAC-A86C1B064CD1_1_201_a.jpeg?csf=1&web=1&e=Lp1rlh"
+            src="https://previews.dropbox.com/p/thumb/AB91Cl2zvoIie0H3P6ADr-JhBofuVlGP5rKG2Fyd22LxQDIYxtsGtiP6WeZbFyHl2BbFnKoQOkq5Qyuew1OXrqNg-aFb9f-SJVMLEmfhmZVM_yBEbvwy1Biu0uJSWfW7RuSlfvnEcU5OAh0rHbTTgRBnln8k06mJVuVWNF5CDubtmWMyp9_5LHGLDzf_Y8BnHECMzmKm4YIQKb2BT2_E3TVNbFfxL5CBxAqbPyDKt1Lemfy5SI9fhu6gA0iGGSZM1rEjGZJQOuB4eWtnrIkZ3EekICgAIgYbOxgYbwkWNL85WGwwuHqoIU8L1akY0VcXU-YA58YMENUP0AdHYl9Tma3L/p.jpeg"
             alt="tracker"
           />
         </div>
